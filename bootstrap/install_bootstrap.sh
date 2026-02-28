b6c96f7cc18543d94802567850d2ea34eb18baa4
Daniel Scholtyssek <daniel.scholtyssek@uk-essen.de>
2026-02-28 20:46:32 +0100

    feat: add ArgoCD Applications for GitOps and reorganize bootstrap scripts into subdirectories

--- bootstrap/install_bootstrap.sh
+++ bootstrap/install_bootstrap.sh
@@ -4,6 +4,18 @@ set -e
 
 # Install bootstrap components
 echo "Installing bootstrap components..."
-./install_metallb.sh
-./install_argocd.sh
+
+# Install MetalLB
+echo "Installing MetalLB..."
+./metallb/install_metallb.sh
+
+# Install ArgoCD
+echo "Installing ArgoCD..."
+./argocd/install_argocd.sh
+
+# Setup ArgoCD GitOps for this repository
+echo "Setting up ArgoCD GitOps..."
+kubectl apply -f argocd/config-application.yaml
+kubectl apply -f argocd/apps-application.yaml
+
 echo "Bootstrap components installed successfully!"
